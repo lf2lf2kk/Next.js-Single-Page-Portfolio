2d9818427cf3465d2dd0113a069ba8c258631e97
Jeremy <lf2lf2kk@gmail.com>
2022-11-01 10:41:26 +1100

    fix image url, update packages

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Axios from "axios";
 
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionTitle } from '../../styles/GlobalComponents';
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,26 +2,26 @@ export const projects = [
 	{
 		title: 'Broccoli Media',
 		description: "My friends and I run a small business, and I make our own official website to promote our serrvices",
-		image: 'https://i.imgur.com/yQ1pyUy.png',
-		tags: ['React', 'Scss', 'Express', 'Node', 'MongoDB'],
+		image: '/images/broccolimedia.png',
+		tags: ['React.js', 'Sass', 'Express.js', 'Node.js', 'MongoDB'],
 		source: '',
 		visit: 'https://broccolimedia.net/',
 		id: 0,
 	},
 	{
 		title: 'MUTPA Club',
-		description: "This is a club introduction website and let Taiwanese students in the University of Melbourne have feelings of family.",
-		image: 'https://i.imgur.com/dD8yWlN.png',
-		tags: ['React', 'CSS', 'Javascript'],
+		description: "This is a club introduction website and unite Taiwanese students in the University of Melbourne",
+		image: '/images/mutpa.png',
+		tags: ['React.js', 'CSS', 'Javascript'],
 		source: 'https://github.com/FuShengK/MUTPA',
 		visit: 'https://mutpa.netlify.app/',
 		id: 1,
 	},
 	{
-		title: 'Trip Advisor Clone Practice',
-		description: "This is a trip searching web application for people who want to know more about the city where they live.",
-		image: '/images/4.jpg',
-		tags: ['React', 'GoogleAPI', 'TripAdvisorAPI'],
+		title: 'Trip Searching website Clone Practice',
+		description: "Made for people who wants to know more about the city where they live.",
+		image: '/images/tripadvisor.png',
+		tags: ['React.js', 'GoogleAPI', 'TripAdvisorAPI'],
 		source: 'https://github.com/FuShengK/TripAdvisorClone',
 		visit: 'https://tripadvisor-practice.netlify.app/',
 		id: 2,
@@ -29,8 +29,8 @@ export const projects = [
 	{
 		title: 'Breaking-Bad Characters Searching',
 		description: "This is a simple website displaying the Breaking-Bad series",
-		image: '/images/3.jpg',
-		tags: ['React', 'BreakingBadAPI'],
+		image: '/images/breakingbad.png',
+		tags: ['React.js', 'BreakingBadAPI'],
 		source: 'https://github.com/FuShengK/Breaking-Bad-Cast',
 		visit: 'https://breakingbad-practice.netlify.app/',
 		id: 3,
@@ -46,8 +46,8 @@ export const TimeLineData = [
 ];
 
 export const AcomplishmentData = [
-	{ number: 5, text: 'Open Source Projects' },
-	{ number: 4, text: 'Internships (Programming-based)', },
+	{ number: 10, text: 'Open Source Projects' },
+	{ number: 1, text: 'Internships (Programming-based)', },
 	{ number: 20, text: 'Certificates', },
-	{ number: 7, text: 'Sponsors', }
+	{ number: 30, text: 'Sponsors', }
 ];
